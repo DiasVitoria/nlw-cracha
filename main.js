cc0400a3ea934c827d32f7c599918b28130090d8
Vitoria Dias <80860267+DiasVitoria@users.noreply.github.com>
2021-10-23 22:29:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const linksSocialMedia = {
+  github: 'DiasVitoria',
+  instagram: 'viimarto',
+  linkedin: 'in/victoria-dias-ab7469208/'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const urlApiGitHub = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(urlApiGitHub)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userGitHub.href = data.html_url
+      profileImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
